1827d45f496aecfed3604241fead5e57e648adac
Nick Dawson <nick@dawson.be>
2021-06-22 22:12:49 +1000

    Send write command WIP

--- connectorbridgepy/connectorbridgepy.py
+++ connectorbridgepy/connectorbridgepy.py
@@ -8,11 +8,23 @@ import socket
 import sys
 
 from Crypto.Cipher import AES
+from enum import Enum
+from types import SimpleNamespace
 
 _logger = logging.getLogger("connectorbridgepy")
 
 # ConnectorBridge
 class ConnectorBridge:
+
+	class MessageType(Enum):
+		GETDEVICELIST = "GetDeviceList"
+		WRITEDEVICE = "WriteDevice"
+
+	class Command(Enum):
+	    CLOSE = 0
+	    OPEN = 1
+	    HALT = 2
+	    STATUS = 3
 
 	class Payload:
 
@@ -32,18 +44,52 @@ class ConnectorBridge:
 			}
 			return json.dumps(payload)
 
+	class WriteDevicePayload(Payload):
+
+		def __init__(self, accessToken, deviceType, mac, data):
+			super().__init__(ConnectorBridge.MessageType.WRITEDEVICE.value)
+
+			self._accessToken = accessToken
+			self._deviceType = deviceType
+			self._mac = mac
+			self._data = data
+
+		def toJSON(self):
+			payload = {
+				"msgType": self._msgType,
+				"msgID": self.msgID(),
+				"AccessToken": self._accessToken,
+				"mac": self._mac,
+				"deviceType": self._deviceType,
+				"data": self._data
+			}
+			return json.dumps(payload)
+		
+
 	# Initialise with the key from Connector app - Settings > About > Tap 4 times
 	def __init__(self, key):
 		_logger.debug("Init with key: " + key)
 
 		self._key = key
 
-	# This displays the payload sent to the hub and the JSON reply
-	def DEBUG(pld,rpy):
-	    print (f"{colour.yellow}PAYLOAD{colour.green}")
-	    print (json.dumps(json.loads(pld),indent=3))
-	    print (f"{colour.yellow}REPLY{colour.green}")
-	    print (json.dumps(rpy,indent=3))
+	# Setup - get device list, token and create access token
+	def setup(self):
+		# self.getDeviceList()
+		self.getInfo()
+		self.refreshToken()
+
+	# Update the access token
+	def refreshToken(self):
+		if self._token is None:
+			_logger.error("No token retrieved from hub")
+			return
+
+		aes = AES.new(self._key, AES.MODE_ECB)
+		encryptedToken = aes.encrypt(self._token)
+
+		self._accessToken = binascii.hexlify(encryptedToken).upper().decode()
+
+		_logger.debug(self._accessToken)
 
 	# Create the UDP socket and send the payload
 	# Timeout is set to one second and will stop if no reply received
@@ -68,6 +114,7 @@ class ConnectorBridge:
 		    udpSocket.close()
 		    _logger.error("Hub is not responding")
 
+	# Get the device list from the hub
 	def getDeviceList(self):
 		# msgID is date/time
 		now = datetime.datetime.now()
@@ -83,6 +130,7 @@ class ConnectorBridge:
 
 		return response
 
+	# Display information about the hub and connected devices
 	def getInfo(self):
 		response = self.getDeviceList()
 
@@ -99,6 +147,11 @@ class ConnectorBridge:
 			for deviceData in response["data"]:
 				print("--> Device type: " + deviceData["deviceType"] + " MAC: " + deviceData["mac"])
 
+	def sendCommand(self, device, command, value):
+		payload = self.WriteDevicePayload(self._accessToken, "10000000", device, {"operation": command.value}).toJSON()
+		print(payload)
+		self.sendUDP(payload)
+
 	def test(self):
 		_logger.debug("Test output: " + self._key)
 		print(self._key)
@@ -114,8 +167,10 @@ def main():
 
 	connector = ConnectorBridge(key="")
 
-	connector.test()
-	connector.getInfo()
+	connector.setup()
+	connector.sendCommand("3c71bf6cf5b8000c", ConnectorBridge.Command.OPEN, 0)
+	# connector.test()
+	# connector.getInfo()
 
 if __name__ == "__main__":
     main()
